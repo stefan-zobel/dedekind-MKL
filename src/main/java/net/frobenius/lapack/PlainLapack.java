c69df4bbf062625ddcf3beede62cafe042a5fccb
stefan-zobel <stefan-zobel@users.noreply.github.com>
2019-10-29 14:03:14 +0100

    sgeev

--- src/main/java/net/frobenius/lapack/PlainLapack.java
+++ src/main/java/net/frobenius/lapack/PlainLapack.java
@@ -657,6 +657,154 @@ public final class PlainLapack {
      *  Purpose
      *  =======
      *
+     *  SGEEV computes for an N-by-N real nonsymmetric matrix A, the
+     *  eigenvalues and, optionally, the left and/or right eigenvectors.
+     *
+     *  The right eigenvector v(j) of A satisfies
+     *                   A * v(j) = lambda(j) * v(j)
+     *  where lambda(j) is its eigenvalue.
+     *  The left eigenvector u(j) of A satisfies
+     *                u(j)**H * A = lambda(j) * u(j)**H
+     *  where u(j)**H denotes the conjugate transpose of u(j).
+     *
+     *  The computed eigenvectors are normalized to have Euclidean norm
+     *  equal to 1 and largest component real.
+     *
+     *  Arguments
+     *  =========
+     *
+     *  JOBVL   (input) CHARACTER*1
+     *          = 'N': left eigenvectors of A are not computed;
+     *          = 'V': left eigenvectors of A are computed.
+     *
+     *  JOBVR   (input) CHARACTER*1
+     *          = 'N': right eigenvectors of A are not computed;
+     *          = 'V': right eigenvectors of A are computed.
+     *
+     *  N       (input) INTEGER
+     *          The order of the matrix A. N >= 0.
+     *
+     *  A       (input/output) REAL array, dimension (LDA,N)
+     *          On entry, the N-by-N matrix A.
+     *          On exit, A has been overwritten.
+     *
+     *  LDA     (input) INTEGER
+     *          The leading dimension of the array A.  LDA >= max(1,N).
+     *
+     *  WR      (output) REAL array, dimension (N)
+     *  WI      (output) REAL array, dimension (N)
+     *          WR and WI contain the real and imaginary parts,
+     *          respectively, of the computed eigenvalues.  Complex
+     *          conjugate pairs of eigenvalues appear consecutively
+     *          with the eigenvalue having the positive imaginary part
+     *          first.
+     *
+     *  VL      (output) REAL array, dimension (LDVL,N)
+     *          If JOBVL = 'V', the left eigenvectors u(j) are stored one
+     *          after another in the columns of VL, in the same order
+     *          as their eigenvalues.
+     *          If JOBVL = 'N', VL is not referenced.
+     *          If the j-th eigenvalue is real, then u(j) = VL(:,j),
+     *          the j-th column of VL.
+     *          If the j-th and (j+1)-st eigenvalues form a complex
+     *          conjugate pair, then u(j) = VL(:,j) + i*VL(:,j+1) and
+     *          u(j+1) = VL(:,j) - i*VL(:,j+1).
+     *
+     *  LDVL    (input) INTEGER
+     *          The leading dimension of the array VL.  LDVL >= 1; if
+     *          JOBVL = 'V', LDVL >= N.
+     *
+     *  VR      (output) REAL array, dimension (LDVR,N)
+     *          If JOBVR = 'V', the right eigenvectors v(j) are stored one
+     *          after another in the columns of VR, in the same order
+     *          as their eigenvalues.
+     *          If JOBVR = 'N', VR is not referenced.
+     *          If the j-th eigenvalue is real, then v(j) = VR(:,j),
+     *          the j-th column of VR.
+     *          If the j-th and (j+1)-st eigenvalues form a complex
+     *          conjugate pair, then v(j) = VR(:,j) + i*VR(:,j+1) and
+     *          v(j+1) = VR(:,j) - i*VR(:,j+1).
+     *
+     *  LDVR    (input) INTEGER
+     *          The leading dimension of the array VR.  LDVR >= 1; if
+     *          JOBVR = 'V', LDVR >= N.
+     *
+     *  WORK    (workspace/output) REAL array, dimension (MAX(1,LWORK))
+     *          On exit, if INFO = 0, WORK(1) returns the optimal LWORK.
+     *
+     *  LWORK   (input) INTEGER
+     *          The dimension of the array WORK.  LWORK >= max(1,3*N), and
+     *          if JOBVL = 'V' or JOBVR = 'V', LWORK >= 4*N.  For good
+     *          performance, LWORK must generally be larger.
+     *
+     *          If LWORK = -1, then a workspace query is assumed; the routine
+     *          only calculates the optimal size of the WORK array, returns
+     *          this value as the first entry of the WORK array, and no error
+     *          message related to LWORK is issued by XERBLA.
+     *
+     *  =====================================================================
+     *
+     * </code>
+     * </pre>
+     *
+     * @param jobvl
+     * @param jobvr
+     * @param n
+     * @param a
+     * @param lda
+     * @param wr
+     * @param wi
+     * @param vl
+     * @param ldvl
+     * @param vr
+     * @param ldvr
+     */
+    public static void sgeev(Lapack la, TEigJob jobvl, TEigJob jobvr, int n, float[] a, int lda, float[] wr, float[] wi,
+            float[] vl, int ldvl, float[] vr, int ldvr) {
+        checkStrictlyPositive(n, "n");
+        checkValueAtLeast(lda, n, "lda");
+        checkMinLen(a, lda * n, "a");
+        checkMinLen(wr, n, "wr");
+        checkMinLen(wi, n, "wi");
+        if (jobvl == TEigJob.ALL) {
+            checkValueAtLeast(ldvl, n, "ldvl");
+            checkMinLen(vl, ldvl * n, "vl");
+        } else {
+            checkValueAtLeast(ldvl, 1, "ldvl");
+        }
+        if (jobvr == TEigJob.ALL) {
+            checkValueAtLeast(ldvr, n, "ldvr");
+            checkMinLen(vr, ldvr * n, "vr");
+        } else {
+            checkValueAtLeast(ldvr, 1, "ldvr");
+        }
+
+        intW info = new intW(0);
+        float[] work = new float[1];
+        la.sgeev(jobvl.val(), jobvr.val(), n, new float[0], lda, new float[0], new float[0], new float[0], ldvl,
+                new float[0], ldvr, work, -1, info);
+        if (info.val != 0) {
+            throwIAEPosition(info);
+        }
+        work = new float[(int) work[0]];
+        la.sgeev(jobvl.val(), jobvr.val(), n, a, lda, wr, wi, vl, ldvl, vr, ldvr, work, work.length, info);
+        if (info.val != 0) {
+            if (info.val < 0) {
+                throwIAEPosition(info);
+            } else {
+                throw new ComputationTruncatedException(
+                        "QR failed to compute all eigenvalues, eigenvectors haven't been computed.");
+            }
+        }
+    }
+
+    /**
+     * <pre>
+     * <code>
+     *
+     *  Purpose
+     *  =======
+     *
      *  DGELQF computes an LQ factorization of a real M-by-N matrix A:
      *  A = L * Q.
      *
