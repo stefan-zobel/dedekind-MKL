4dd73004d46cd8c16f2c6a24bfd3a9da459a3c6b
stefan-zobel <stefan-zobel@users.noreply.github.com>
2020-08-31 01:06:55 +0200

    introduce BLAS extensions

---
+++ src/main/java/net/dedekind/blas/BlasExt.java
@@ -0,0 +1,260 @@
+/*
+ * Copyright 2020 Stefan Zobel
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package net.dedekind.blas;
+
+import java.util.Objects;
+
+import net.dedekind.Order;
+import net.frobenius.TTrans;
+
+/**
+ * {@code Intel MKL} (Math Kernel Library) native implementations of selected
+ * BLAS extensions.
+ * <p>
+ * Matrix storage layout must be column-major as in Fortran. The number of
+ * matrix rows and columns must always be strictly positive. All operations
+ * throw a {@code NullPointerException} if any of the reference method arguments
+ * is {@code null}.
+ */
+public class BlasExt {
+
+    // The ordering is column-major
+    private static final byte ORDERING = (byte) 'C';
+
+    private static final boolean USE_CRITICAL = true;
+
+    private static final BlasExt mkl;
+
+    static {
+        Blas.getInstance(true);
+        mkl = new BlasExt();
+    }
+
+    public static BlasExt getInstance() {
+        return mkl;
+    }
+
+    public final void cgemm3m(Trans transa, Trans transb, int m, int n, int k, float alphar, float alphai, float[] a,
+            int lda, float[] b, int ldb, float betar, float betai, float[] c, int ldc) {
+        Objects.requireNonNull(a, "a");
+        Objects.requireNonNull(b, "b");
+        Objects.requireNonNull(c, "c");
+        Objects.requireNonNull(transa, "transa");
+        Objects.requireNonNull(transb, "transb");
+        checkStrictlyPositive(m, "m");
+        checkStrictlyPositive(n, "n");
+        checkStrictlyPositive(k, "k");
+        cgemm3m_n(Order.COL.code(), transa.code(), transb.code(), m, n, k, alphar, alphai, a, lda, b, ldb, betar, betai,
+                c, ldc, USE_CRITICAL);
+    }
+
+    public final void zgemm3m(Trans transa, Trans transb, int m, int n, int k, double alphar, double alphai, double[] a,
+            int lda, double[] b, int ldb, double betar, double betai, double[] c, int ldc) {
+        Objects.requireNonNull(a, "a");
+        Objects.requireNonNull(b, "b");
+        Objects.requireNonNull(c, "c");
+        Objects.requireNonNull(transa, "transa");
+        Objects.requireNonNull(transb, "transb");
+        checkStrictlyPositive(m, "m");
+        checkStrictlyPositive(n, "n");
+        checkStrictlyPositive(k, "k");
+        zgemm3m_n(Order.COL.code(), transa.code(), transb.code(), m, n, k, alphar, alphai, a, lda, b, ldb, betar, betai,
+                c, ldc, USE_CRITICAL);
+    }
+
+    public final void simatcopy(TTrans trans, int rows, int cols, float alpha, float[] AB, int lda, int ldb) {
+        Objects.requireNonNull(trans, "trans");
+        Objects.requireNonNull(AB, "AB");
+        checkStrictlyPositive(rows, "rows");
+        checkStrictlyPositive(cols, "cols");
+        simatcopy_n(ORDERING, trans(trans), rows, cols, alpha, AB, lda, ldb, USE_CRITICAL);
+    }
+
+    public final void dimatcopy(TTrans trans, int rows, int cols, double alpha, double[] AB, int lda, int ldb) {
+        Objects.requireNonNull(trans, "trans");
+        Objects.requireNonNull(AB, "AB");
+        checkStrictlyPositive(rows, "rows");
+        checkStrictlyPositive(cols, "cols");
+        dimatcopy_n(ORDERING, trans(trans), rows, cols, alpha, AB, lda, ldb, USE_CRITICAL);
+    }
+
+    public final void cimatcopy(TTrans trans, int rows, int cols, float alphar, float alphai, float[] AB, int lda,
+            int ldb) {
+        Objects.requireNonNull(trans, "trans");
+        Objects.requireNonNull(AB, "AB");
+        checkStrictlyPositive(rows, "rows");
+        checkStrictlyPositive(cols, "cols");
+        cimatcopy_n(ORDERING, trans(trans), rows, cols, alphar, alphai, AB, lda, ldb, USE_CRITICAL);
+    }
+
+    public final void zimatcopy(TTrans trans, int rows, int cols, double alphar, double alphai, double[] AB, int lda,
+            int ldb) {
+        Objects.requireNonNull(trans, "trans");
+        Objects.requireNonNull(AB, "AB");
+        checkStrictlyPositive(rows, "rows");
+        checkStrictlyPositive(cols, "cols");
+        zimatcopy_n(ORDERING, trans(trans), rows, cols, alphar, alphai, AB, lda, ldb, USE_CRITICAL);
+    }
+
+    public final void somatcopy(TTrans trans, int rows, int cols, float alpha, float[] A, int lda, float[] B, int ldb) {
+        Objects.requireNonNull(trans, "trans");
+        Objects.requireNonNull(A, "A");
+        Objects.requireNonNull(B, "B");
+        checkStrictlyPositive(rows, "rows");
+        checkStrictlyPositive(cols, "cols");
+        somatcopy_n(ORDERING, trans(trans), rows, cols, alpha, A, lda, B, ldb, USE_CRITICAL);
+    }
+
+    public final void domatcopy(TTrans trans, int rows, int cols, double alpha, double[] A, int lda, double[] B,
+            int ldb) {
+        Objects.requireNonNull(trans, "trans");
+        Objects.requireNonNull(A, "A");
+        Objects.requireNonNull(B, "B");
+        checkStrictlyPositive(rows, "rows");
+        checkStrictlyPositive(cols, "cols");
+        domatcopy_n(ORDERING, trans(trans), rows, cols, alpha, A, lda, B, ldb, USE_CRITICAL);
+    }
+
+    public final void comatcopy(TTrans trans, int rows, int cols, float alphar, float alphai, float[] A, int lda,
+            float[] B, int ldb) {
+        Objects.requireNonNull(trans, "trans");
+        Objects.requireNonNull(A, "A");
+        Objects.requireNonNull(B, "B");
+        checkStrictlyPositive(rows, "rows");
+        checkStrictlyPositive(cols, "cols");
+        comatcopy_n(ORDERING, trans(trans), rows, cols, alphar, alphai, A, lda, B, ldb, USE_CRITICAL);
+    }
+
+    public final void zomatcopy(TTrans trans, int rows, int cols, double alphar, double alphai, double[] A, int lda,
+            double[] B, int ldb) {
+        Objects.requireNonNull(trans, "trans");
+        Objects.requireNonNull(A, "A");
+        Objects.requireNonNull(B, "B");
+        checkStrictlyPositive(rows, "rows");
+        checkStrictlyPositive(cols, "cols");
+        zomatcopy_n(ORDERING, trans(trans), rows, cols, alphar, alphai, A, lda, B, ldb, USE_CRITICAL);
+    }
+
+    public final void somatadd(TTrans transa, TTrans transb, int m, int n, float alpha, float[] A, int lda, float beta,
+            float[] B, int ldb, float[] C, int ldc) {
+        Objects.requireNonNull(transa, "transa");
+        Objects.requireNonNull(transb, "transb");
+        Objects.requireNonNull(A, "A");
+        Objects.requireNonNull(B, "B");
+        Objects.requireNonNull(C, "C");
+        checkStrictlyPositive(m, "m");
+        checkStrictlyPositive(n, "n");
+        somatadd_n(ORDERING, trans(transa), trans(transb), m, n, alpha, A, lda, beta, B, ldb, C, ldc, USE_CRITICAL);
+    }
+
+    public final void domatadd(TTrans transa, TTrans transb, int m, int n, double alpha, double[] A, int lda,
+            double beta, double[] B, int ldb, double[] C, int ldc) {
+        Objects.requireNonNull(transa, "transa");
+        Objects.requireNonNull(transb, "transb");
+        Objects.requireNonNull(A, "A");
+        Objects.requireNonNull(B, "B");
+        Objects.requireNonNull(C, "C");
+        checkStrictlyPositive(m, "m");
+        checkStrictlyPositive(n, "n");
+        domatadd_n(ORDERING, trans(transa), trans(transb), m, n, alpha, A, lda, beta, B, ldb, C, ldc, USE_CRITICAL);
+    }
+
+    public final void comatadd(TTrans transa, TTrans transb, int m, int n, float alphar, float alphai, float[] A,
+            int lda, float betar, float betai, float[] B, int ldb, float[] C, int ldc) {
+        Objects.requireNonNull(transa, "transa");
+        Objects.requireNonNull(transb, "transb");
+        Objects.requireNonNull(A, "A");
+        Objects.requireNonNull(B, "B");
+        Objects.requireNonNull(C, "C");
+        checkStrictlyPositive(m, "m");
+        checkStrictlyPositive(n, "n");
+        comatadd_n(ORDERING, trans(transa), trans(transb), m, n, alphar, alphai, A, lda, betar, betai, B, ldb, C, ldc,
+                USE_CRITICAL);
+    }
+
+    public final void zomatadd(TTrans transa, TTrans transb, int m, int n, double alphar, double alphai, double[] A,
+            int lda, double betar, double betai, double[] B, int ldb, double[] C, int ldc) {
+        Objects.requireNonNull(transa, "transa");
+        Objects.requireNonNull(transb, "transb");
+        Objects.requireNonNull(A, "A");
+        Objects.requireNonNull(B, "B");
+        Objects.requireNonNull(C, "C");
+        checkStrictlyPositive(m, "m");
+        checkStrictlyPositive(n, "n");
+        zomatadd_n(ORDERING, trans(transa), trans(transb), m, n, alphar, alphai, A, lda, betar, betai, B, ldb, C, ldc,
+                USE_CRITICAL);
+    }
+
+    private static native void cgemm3m_n(int order, int transa, int transb, int m, int n, int k, float alphar,
+            float alphai, float[] a, int lda, float[] b, int ldb, float betar, float betai, float[] c, int ldc,
+            boolean useCriticalRegion);
+
+    private static native void zgemm3m_n(int order, int transa, int transb, int m, int n, int k, double alphar,
+            double alphai, double[] a, int lda, double[] b, int ldb, double betar, double betai, double[] c, int ldc,
+            boolean useCriticalRegion);
+
+    private static native void simatcopy_n(byte ordering, byte trans, int rows, int cols, float alpha, float[] AB,
+            int lda, int ldb, boolean useCriticalRegion);
+
+    private static native void dimatcopy_n(byte ordering, byte trans, int rows, int cols, double alpha, double[] AB,
+            int lda, int ldb, boolean useCriticalRegion);
+
+    private static native void cimatcopy_n(byte ordering, byte trans, int rows, int cols, float alphar, float alphai,
+            float[] AB, int lda, int ldb, boolean useCriticalRegion);
+
+    private static native void zimatcopy_n(byte ordering, byte trans, int rows, int cols, double alphar, double alphai,
+            double[] AB, int lda, int ldb, boolean useCriticalRegion);
+
+    private static native void somatcopy_n(byte ordering, byte trans, int rows, int cols, float alpha, float[] A,
+            int lda, float[] B, int ldb, boolean useCriticalRegion);
+
+    private static native void domatcopy_n(byte ordering, byte trans, int rows, int cols, double alpha, double[] A,
+            int lda, double[] B, int ldb, boolean useCriticalRegion);
+
+    private static native void comatcopy_n(byte ordering, byte trans, int rows, int cols, float alphar, float alphai,
+            float[] A, int lda, float[] B, int ldb, boolean useCriticalRegion);
+
+    private static native void zomatcopy_n(byte ordering, byte trans, int rows, int cols, double alphar, double alphai,
+            double[] A, int lda, double[] B, int ldb, boolean useCriticalRegion);
+
+    private static native void somatadd_n(byte ordering, byte transa, byte transb, int m, int n, float alpha, float[] A,
+            int lda, float beta, float[] B, int ldb, float[] C, int ldc, boolean useCriticalRegion);
+
+    private static native void domatadd_n(byte ordering, byte transa, byte transb, int m, int n, double alpha,
+            double[] A, int lda, double beta, double[] B, int ldb, double[] C, int ldc, boolean useCriticalRegion);
+
+    private static native void comatadd_n(byte ordering, byte transa, byte transb, int m, int n, float alphar,
+            float alphai, float[] A, int lda, float betar, float betai, float[] B, int ldb, float[] C, int ldc,
+            boolean useCriticalRegion);
+
+    private static native void zomatadd_n(byte ordering, byte transa, byte transb, int m, int n, double alphar,
+            double alphai, double[] A, int lda, double betar, double betai, double[] B, int ldb, double[] C, int ldc,
+            boolean useCriticalRegion);
+
+    private static byte trans(TTrans trans) {
+        return (byte) Character.toUpperCase(trans.val().charAt(0));
+    }
+
+    private static void checkStrictlyPositive(int value, String name) {
+        if (value <= 0) {
+            throw new IllegalArgumentException(
+                    "Parameter " + name + " must be strictly positive (value = " + value + ")");
+        }
+    }
+
+    protected BlasExt() {
+    }
+}
