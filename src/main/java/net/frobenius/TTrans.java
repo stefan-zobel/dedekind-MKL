37f5f7d18a49678c5e524bc29da7f2d803e84227
stefan-zobel <stefan-zobel@users.noreply.github.com>
2020-08-31 01:06:27 +0200

    add CONJ("R") for BLAS extensions

--- src/main/java/net/frobenius/TTrans.java
+++ src/main/java/net/frobenius/TTrans.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019 Stefan Zobel
+ * Copyright 2019, 2020 Stefan Zobel
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -19,7 +19,8 @@ public enum TTrans {
 
     NO_TRANS("N"), //
     TRANS("T"), //
-    CONJ_TRANS("C"); //
+    CONJ_TRANS("C"), //
+    CONJ("R"); //
 
     private final String val;
 
